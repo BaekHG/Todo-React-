43764db760abdd74a9f19a9d5df1892a7b7ef2da
brobaik <hhyungi94@gmail.com>
2020-05-26 17:19:38 +0900

    add port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.get('/api/greeting', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
 ReactDOM.render(
   <React.StrictMode>
     <App />
