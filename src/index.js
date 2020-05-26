85552a3729569987a15580436ba1f9af077af1af
brobaik <hhyungi94@gmail.com>
2020-05-26 17:37:03 +0900

    add engine

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/api/greeting', (req, res) => {
-  res.send('Hello World!');
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
 ReactDOM.render(
   <React.StrictMode>
     <App />
